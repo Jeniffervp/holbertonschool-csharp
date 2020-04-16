b5a9ccf260179898e72db6c65ad9098b9ff861fa
jeniffervp <737@holbertonschool.com>
2020-04-16 12:19:42 -0500

    [ADD]print array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            return null;
+        }
+
+        int[] numbers = new int[size];
+
+        if (size > 0)
+        {
+            for (int i = 0; i < size; i++)
+            {
+		Console.Write("{0}", i);
+                if (i < size - 1)
+                {
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write("\n");
+                }
+            }
+        }
+        else if (size == 0)
+        {
+            Console.Write("\n");
+        }
+	return numbers;
+    }
+}
