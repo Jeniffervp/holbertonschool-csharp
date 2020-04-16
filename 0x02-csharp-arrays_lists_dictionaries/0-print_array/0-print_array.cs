a2f60a7696bdea81148f917600953d11f927ae77
jeniffervp <737@holbertonschool.com>
2020-04-16 12:29:41 -0500

    [Fix]typo

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -6,7 +6,7 @@ class Array
     {
         if (size < 0)
         {
-            Console.WritheLine("Size cannot be negative");
+            Console.WriteLine("Size cannot be negative");
             return null;
         }
 
