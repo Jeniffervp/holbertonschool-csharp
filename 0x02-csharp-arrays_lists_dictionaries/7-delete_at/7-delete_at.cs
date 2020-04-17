9a2ab56a1411c9fc7200eaac151c5a2d6521dbc5
jeniffervp <737@holbertonschool.com>
2020-04-16 19:05:46 -0500

    [Fix]>=

--- 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -5,18 +5,12 @@ class List
 {
     public static List<int> DeleteAt(List<int> myList, int index)
     {
-        if (index < 0 || index > myList.Count)
+        if (index < 0 || index >= myList.Count)
         {
             Console.WriteLine("Index is out of range");
 	    return myList;
         }
-        for (int i = 0; i < myList.Count; i++)
-        {
-            if (i == index)
-            {
-                myList.Remove(myList[index]);
-            }
-        }
+        myList.Remove(myList[index]);
 	return myList;
     }
 }
