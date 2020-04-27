ec435b732cce09223c7205441dc8453107f33dd5
Jeniffervp <737@holbertonschool.com>
2020-04-27 18:49:43 -0500

    [Fix] adding double quotes

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -7,7 +7,7 @@ class MyStack
     {
         Console.WriteLine("Number of items: {0}", aStack.Count);
         Console.WriteLine((aStack.Count > 0) ? "Top item: {0}" : "Stack is empty", aStack.Peek());
-        Console.WriteLine("Stack contains {0}: {1}", search, aStack.Contains(search) ? "True" : "False");
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, aStack.Contains(search) ? "True" : "False");
         while (aStack.Pop() != search);
         aStack.Push(newItem);
         return aStack;
