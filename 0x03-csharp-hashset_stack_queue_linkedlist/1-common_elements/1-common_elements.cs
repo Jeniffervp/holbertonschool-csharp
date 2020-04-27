0b45bffe8bf7c50fa0c70a8220cc95370eb2890e
Jeniffervp <737@holbertonschool.com>
2020-04-27 14:59:26 -0500

    [Add] duplicate values

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> result = new List<int>();
+        foreach (int a in list1) 
+        {
+            foreach (int b in list2)
+            {
+                if (a == b)
+                {
+                    result.Add(a);
+                }
+            }
+        }
+        result.Sort();
+        return result;
+    }
+}
--- run.sh
+++ run.sh
@@ -2,4 +2,3 @@
 dotnet new console -n $1                                                   
 cd $1                                                                      
 mv Program.cs $1.cs                                                        
-emacs $1.cs
