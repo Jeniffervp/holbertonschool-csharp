a045be139a0df6c239717fcb2b78d58e456de1ad
Jeniffervp <737@holbertonschool.com>
2020-04-27 14:35:52 -0500

    [Add] sum

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash                                                        
+dotnet new console -n $1                                                   
+cd $1                                                                      
+mv Program.cs $1.cs                                                        
+emacs $1.cs
