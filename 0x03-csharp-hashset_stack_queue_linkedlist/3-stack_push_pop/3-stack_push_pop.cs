16abf79231fbae68a771da004198777492c8764b
Jeniffervp <737@holbertonschool.com>
2020-04-27 19:10:37 -0500

    [Fix] Conditional

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -6,7 +6,10 @@ class MyStack
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
         Console.WriteLine("Number of items: {0}", aStack.Count);
-        Console.WriteLine((aStack.Count > 0) ? "Top item: {0}" : "Stack is empty", aStack.Peek());
+        if (aStack.Count > 0)
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+        else
+            Console.WriteLine("Stack is empty");
         Console.WriteLine("Stack contains \"{0}\": {1}", search, aStack.Contains(search) ? "True" : "False");
         if (aStack.Contains(search))
             while (aStack.Pop() != search);
