f1c361004f8b25202fe4eb63b51651888c8ae3b1
Jeniffervp <737@holbertonschool.com>
2020-05-04 22:59:10 -0500

    [Add] Test cases

---
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,48 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+namespace MyMath.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void First_Test()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public void List_Empty()
+        {
+            List<int> num = null;
+            int result = Operations.Max(num);
+            Assert.AreEqual(0, result);
+        }
+        [Test]
+        public void Max_In_List()
+        {
+            List<int> nums = new List<int> {5, 2, 7, 9, 3, 1};
+            int result = Operations.Max(nums);
+            Assert.AreEqual(9, result);
+        }
+        [Test]
+        public void Neg_Int_List()
+        {
+            List<int> num = new List<int> {-5, -2, -7, -9, -3, -1};
+            int result = Operations.Max(num);
+            Assert.AreEqual(-1, result);
+        }
+        [Test]
+        public void Mix_Int_List()
+        {
+            List<int> num = new List<int> {-5, 2, -7, 9, 3, -1};
+            int result = Operations.Max(num);
+            Assert.AreEqual(9, result);
+        }
+        [Test]
+        public void Zero_In_List()
+        {
+            List<int> num = new List<int> {-5, -2, -7, 0, -3, -1};
+            int result = Operations.Max(num);
+            Assert.AreEqual(0, result);
+        }
+    }
+}
