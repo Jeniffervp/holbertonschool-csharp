2e1bf16f11594eb049bbf046da138110df38bfc3
Jeniffervp <737@holbertonschool.com>
2020-05-27 15:52:57 -0500

    [fix] change class

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 
-class Program {
-    public static double Magnitude(double[] vector) {
+class VectorMath {
+    
+    public static double Magnitude(double[] vector) 
+    {
         if (vector.Length < 2 || vector.Length > 3)
             return (-1);
 
         double res = 0;
 
-        for (int i = 0; i < vector.Length; i++) {
+        for (int i = 0; i < vector.Length; i++) 
+        {
             res = res + (vector[i] * vector[i]);
         }
         return (Math.Round(Math.Sqrt(res), 2));
