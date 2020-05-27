89bde29e417f5e6d20febaa6a32a7979ae15f1c6
Jeniffervp <737@holbertonschool.com>
2020-05-27 15:47:04 -0500

    [fix] length of vectors

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -2,12 +2,12 @@
 
 class Program {
     public static double Magnitude(double[] vector) {
-        if (vector < 2 || vector > 3)
+        if (vector.Length < 2 || vector.Length > 3)
             return (-1);
 
         double res = 0;
 
-        for (int i = 0; i < Lenght(vector); i++) {
+        for (int i = 0; i < vector.Length; i++) {
             res = res + (vector[i] * vector[i]);
         }
         return (Math.Round(Math.Sqrt(res), 2));
