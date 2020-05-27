99fbc52620a206c80137ee5945f8d7bdc479a603
Jeniffervp <737@holbertonschool.com>
2020-05-27 17:02:06 -0500

    [Add] multiply vector by an scalar

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        double[] wrong = new double[1] {-1};
+        if ((vector.Length < 2 || vector.Length > 3))
+            return (wrong);
+
+        double[] mul = new double[vector.Length];
+
+        for (int i = 0; i < vector.Length; i++)
+        {
+            mul[i] = vector[i] * scalar;
+        }
+        return (mul);
+    }
+}
