2e3065fcbdd716d2cca386039cd109877ab97527
Jeniffervp <737@holbertonschool.com>
2020-05-27 17:50:18 -0500

    [Add] dot product

---
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        if ((vector1.Length < 2 || vector1.Length > 3) && (vector2.Length < 2 || vector2.Length > 3) || (vector1.Length != vector2.Length))
+        {
+            return (-1.0);
+        }
+        double product = 0;
+
+        for (int i = 0; i < vector1.Length && i < vector2.Length; i++)
+        {
+            product = product + vector1[i] * vector2[i];
+        }
+        return (product);
+    }
+}
