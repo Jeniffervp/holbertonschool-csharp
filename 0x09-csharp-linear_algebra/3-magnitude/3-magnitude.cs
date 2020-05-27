7eeff810d57a04b91f46d8babdf62ab95edd0882
Jeniffervp <737@holbertonschool.com>
2020-05-27 15:13:36 -0500

    [Add] find the hipotenusa

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program {
+    public static double Magnitude (double[] vector) {
+        if (vector < 2 || vector > 3)
+            return (-1);
+
+        double res = 0;
+
+        for (int i = 0; i < Lenght (vector); i++) {
+            res = res + (vector[i] * vector[i]);
+        }
+        return (Math.Round (Math.Sqrt (res), 2));
+    }
+}
