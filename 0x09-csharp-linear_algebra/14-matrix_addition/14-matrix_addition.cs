8c7005fca03a7e5c822d4cbeb3efb36d145f93f2
Jeniffervp <737@holbertonschool.com>
2020-05-27 20:55:33 -0500

    [fix] conditional

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -5,7 +5,7 @@ class MatrixMath
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
         double[,] wrong = new double[,] {{-1}};
-        if ((matrix1.GetLength(0) < 2 || matrix1.GetLength(0) > 3) || (matrix2.GetLength(0) < 2 || matrix2.GetLength(0) > 3) || (matrix1.GetLength(0) != matrix2.GetLength(0)))
+        if ((matrix1.GetLength(0) < 2 || matrix1.GetLength(0) > 3) || (matrix2.GetLength(0) < 2 || matrix2.GetLength(0) > 3) || (matrix1.GetLength(0) != matrix2.GetLength(0)) || (matrix1.GetLength(0) != matrix1.GetLength(1)) || matrix2.GetLength(0) != matrix2.GetLength(1))
             return (wrong);
 
         double[,] sum = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
