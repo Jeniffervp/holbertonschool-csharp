0f90a6b74f2abeafeb7fee6fe4748bd848b9a252
Jeniffervp <737@holbertonschool.com>
2020-05-28 00:29:13 -0500

    [fix] rounded

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -8,16 +8,19 @@ class MatrixMath
         double[,] matrix1 = new double[,] {{Math.Cos(angle), Math.Sin(angle)}, {-1 * Math.Sin(angle), Math.Cos(angle)}};
         double oper;
         double[,] res = new double[2, 2];
-        if (matrix.GetLength(0) != 2 && matrix.GetLength(1) != 2)
+
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
             return wrong;
+
         for (int i = 0; i < 2; i++)
             for (int j = 0; j < 2; j++)
             {
                 oper = 0;
                 for (int k = 0; k < 2; k++)
-                    oper = oper + Math.Round((matrix[i, k] * matrix1[k, j]), 2);
+                    oper = Math.Round(oper + (matrix[i, k] * matrix1[k, j]), 2);
                 res[i, j] = oper;
             }
+
         return res;
     }
 }
