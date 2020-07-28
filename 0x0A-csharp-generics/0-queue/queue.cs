67450821670956684a5e804faeecda61be5be9a5
Jeniffer Vanegas <jeniffervanegas.habi.co>
2020-07-27 20:34:51 -0500

    [Fix] comments and semicolon

--- 0x0A-csharp-generics/0-queue/queue.cs
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
-namespace _0_queue
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+/// <summary> Class that doesn't inherit </summary>
+/// <typeparam name="T"> parameter </typeparam>
+class Queue<T> {
+
+    /// <summary>Function to check the type of Queue</summary>
+    /// <returns></returns>
+
+    public Type CheckType () {
+        return typeOf (T);
     }
 }
