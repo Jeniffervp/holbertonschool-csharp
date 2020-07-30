06508c30492aa1632730ad09227c23ea19100069
Jeniffer Vanegas <jeniffervanegas.habi.co>
2020-07-29 20:49:17 -0500

    [fix] spaces

--- 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -3,30 +3,30 @@
 /// <summary> Class Player </summary>
 public class Player
 {
-    private string name;
-    private float maxHp;
-    private float hp;
+  private string name;
+  private float maxHp;
+  private float hp;
 
-    /// <summary> Constructor to set value </summary>
-    /// <param name="name"> Player's Name </param>
-    /// <param name="maxHp"> Max health </param>
-    public Player(string name = "Player", float maxHp = 100f)
+  /// <summary> Constructor to set value </summary>
+  /// <param name="name"> Player's Name </param>
+  /// <param name="maxHp"> Max health </param>
+  public Player(string name = "Player", float maxHp = 100f)
+  {
+    if (maxHp <= 0)
     {
-        if (maxHp <= 0)
-        {
-            this.maxHp = 100f;
-            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
-        }
-        else
-            this.maxHp = maxHp;
-
-        this.hp = this.maxHp;
-        this.name = name;
+      this.maxHp = 100f;
+      Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
     }
+    else
+      this.maxHp = maxHp;
 
-    /// <summary> Print default val </summary>
-    public void PrintHealth()
-    {
-        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
-    }
+    this.hp = this.maxHp;
+    this.name = name;
+  }
+
+  /// <summary> Print default val </summary>
+  public void PrintHealth()
+  {
+    Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+  }
 }
